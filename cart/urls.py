7b5a93071a2b3d0ccc9ddaf303e21bdfffc44ef9
oriar <38orlov@gmail.com>
2023-06-01 16:28:18 +0500

    change CartAddView

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from cart.views import cart_add, cart_update, cart_remove, CartView
+from cart.views import CartAddView, cart_update, cart_remove, CartView
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart_detail'),
-    path('add/', cart_add, name='cart_add'),
+    path('add/', CartAddView.as_view(), name='cart_add'),
     path('update/<str:sku>', cart_update, name='cart_update'),
     path('remove/<str:sku>', cart_remove, name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -4,31 +4,55 @@ from django.db.models import Sum, F
 from django.forms import formset_factory
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 
 from cart.models import Cart
 from cart.forms import CartAddProductForm, CartUpdateProductForm, BaseAddProductFormSet
 from core.models import ProductPack
 
 
-@login_required
-@require_POST
-def cart_add(request):
-    ProductFormSet = formset_factory(CartAddProductForm,
-                                     formset=BaseAddProductFormSet)
-    formset = ProductFormSet(request.POST)
-    if formset.is_valid():
-        cd = formset.cleaned_data
-        for item in cd:
-            if item.get('quantity'):
-                pack = ProductPack.objects.get(sku=item.get('sku'))
-                cart, _ = Cart.objects.get_or_create(user=request.user,
-                                                     pack=pack,
-                                                     defaults={'quantity': 0})
-                cart.quantity += item.get('quantity')
-                cart.save()
+class CartView(LoginRequiredMixin, ListView):
 
-    return redirect('cart:cart_detail')
+    template_name = 'cart/cart.html'
+
+    def get_queryset(self):
+        user_cart = Cart.objects.filter(user=self.request.user).\
+            select_related('pack__product')
+        return user_cart
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # total_price = sum([item.get_total_price() for item in self.object_list])
+
+        total_price = self.object_list.aggregate(tp=Sum(F('quantity') *
+                                                        F('pack__price') *
+                                                        F('pack__items_in_box'))).\
+            get('tp')
+
+        context['total_price'] = total_price
+        context['title'] = 'Корзина'
+        return context
+
+
+class CartAddView(LoginRequiredMixin, View):
+
+    def post(self, request):
+
+        FormSet = formset_factory(CartAddProductForm, formset=BaseAddProductFormSet)
+        formset = FormSet(request.POST)
+
+        if formset.is_valid():
+            cd = formset.cleaned_data
+            for item in cd:
+                if item.get('quantity'):
+                    pack = ProductPack.objects.get(sku=item.get('sku'))
+                    cart, _ = Cart.objects.get_or_create(user=request.user,
+                                                         pack=pack,
+                                                         defaults={'quantity': 0})
+                    cart.quantity += item.get('quantity')
+                    cart.save()
+        return redirect('cart:cart_detail')
 
 
 @login_required
@@ -52,26 +76,3 @@ def cart_remove(request, sku):
     cart.delete()
 
     return redirect('cart:cart_detail')
-
-
-class CartView(LoginRequiredMixin, ListView):
-
-    template_name = 'cart/cart.html'
-
-    def get_queryset(self):
-        user_cart = Cart.objects.filter(user=self.request.user).\
-            select_related('pack__product')
-        return user_cart
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # total_price = sum([item.get_total_price() for item in self.object_list])
-        total_price = self.object_list.aggregate(tp=Sum(F('quantity') *
-                                                        F('pack__price') *
-                                                        F('pack__items_in_box'))).\
-            get('tp')
-
-        context['total_price'] = total_price
-        context['title'] = 'Корзина'
-        return context
