23fd820031d17e7e3ff84cd3b2d4e4c636a917f8
oriar <38orlov@gmail.com>
2023-06-02 15:15:47 +0500

    query optimization

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS',
 if DEBUG:
     import socket
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1",
+                                                                 "10.0.2.2"]
 
 # Application definition
 
